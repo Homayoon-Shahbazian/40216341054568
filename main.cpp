0f3a73e3ccf59134b990c583a2a9abdffb48279c
Homayoon-Shahbazian <155094761+Homayoon-Shahbazian@users.noreply.github.com>
2023-12-28 21:25:11 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
-
-    cout << "Enter the three numbers: ";
-    cin >> a >> b >> c;
-
-    // Checking the conditions for a right-angled triangle
-    if (a * a + b * b == c * c ||a * a + c * c == b * b|| b * b + c * c == a * a) {
-        cout << "These numbers can form a right-angled triangle" << endl;
-    } else {
-        cout << "These numbers cannot form a right-angled triangle" << endl;
-    }
-
+    cout << "Hello" << endl;
     return 0;
 }
