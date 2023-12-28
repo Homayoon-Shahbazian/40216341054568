a06fed561180010304bc1e2b2640cf45757070e6
Homayoon-Shahbazian <155094761+Homayoon-Shahbazian@users.noreply.github.com>
2023-12-28 21:11:18 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a,b,c;
-    cout<<"Enter 3 number";
-    cin>>a>>b>>c;
-    if(c*c==a*a+b*b||b*b==a*a+c*c||a*a==b*b+c*c){
-        cout<<"True";
-        } else{
-        cout<<"False";
-    }return 0;
+
+int main() {
+    int num1, num2, num3;
+
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    if (num1 > num2) {
+        if (num1 > num3) {
+            cout << "The largest number is: " << num1 << endl;
+        } else {
+            cout << "The largest number is: " << num3 << endl;
+        }
+    } else {
+        if (num2 > num3) {
+            cout << "The largest number is: " << num2 << endl;
+        } else {
+            cout << "The largest number is: " << num3 << endl;
+        }
+    }
+
+    return 0;
 }
